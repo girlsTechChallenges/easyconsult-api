70939e1fe3c3a8b9337a2307b8d6fa290fbd8931
Agatha Paiva <agathahost@gmail.com>
2025-09-01 22:06:29 -0300

    gitignore

---
+++ src/main/java/com/fiap/easyconsult/EasyconsultMain.java
@@ -0,0 +1,13 @@
+package com.fiap.easyconsult;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyconsultMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyconsultMain.class, args);
+	}
+
+}
---
+++ src/test/java/com/fiap/easyconsult/EasyconsultMainTests.java
@@ -0,0 +1,13 @@
+package com.fiap.easyconsult;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyconsultMainTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
